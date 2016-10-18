99a1404119a72abc777643572ad0b8dc604d846b
Daria Serdiuk <daria_serdiuk@epam.com>
2016-10-18 16:26:19 +0300

    1.100: updated task working with binaries

--- task100.py
+++ task100.py
@@ -1,44 +1,26 @@
 START_CYRILLIC_CODE = 1040
 
 
-def get_code(char):
-    code = 0 if char.isspace() else ord(char) - START_CYRILLIC_CODE + 1
-    return to_binary6(code)
-
-
-def to_binary6(decimal):
-    return "{0:06b}".format(decimal)
-
-
-def to_binary12(decimal):
-    return "{0:012b}".format(decimal)
-
-
-def to_decimal(binary):
-    return int(binary, 2)
-
-
 def cipher(message):
-    result = []
-    for i in range(0, len(message) - 1, 2):
-        binary = get_code(message[i]) + get_code(message[i + 1])
-        result.append(to_decimal(binary))
-    return result
+    if len(message) % 2 != 0:
+        message += ' '
+    return [code(message[i]) << 6 | code(message[i + 1]) for i in range(0, len(message) - 1, 2)]
 
 
-def get_char(binary):
-    return chr(to_decimal(binary) + START_CYRILLIC_CODE - 1)
+def code(char):
+    return 0 if char.isspace() else ord(char) - START_CYRILLIC_CODE + 1
 
 
+def char(code):
+    return ' ' if not code else chr(code + START_CYRILLIC_CODE - 1)
+
+
+# 0x3F is 0b111111
 def decipher(message):
-    result = ""
-    for i in message:
-        binary = to_binary12(i)
-        middle = int(len(binary) / 2)
-        result += get_char(binary[0:middle]) + get_char(binary[middle:len(binary)])
-    return result
+    print(message[3] >> 6)
+    print(message[3] & 0x3F)
+    return ''.join((char(i >> 6) + char(i & 0x3F)) for i in message)
 
 
 if __name__ == "__main__":
-    # consider odd word length
-    print(decipher(cipher('СЕРВЕРР')))
+    print(decipher(cipher('СЕРВЕРЗ')))
