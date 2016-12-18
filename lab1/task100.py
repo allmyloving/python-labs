bc28d69c746065c14a5de957ec6fff0e90d05390
Daria Serdiuk <daria.serdiuk13@gmail.com>
2016-12-18 15:31:45 +0200

    moved all lab1 files to separate folder

--- lab1/task100.py
+++ lab1/task100.py
@@ -2,15 +2,18 @@ START_CYRILLIC_CODE = 1040
 
 
 def cipher(message):
+    # add redundant space sign if the input has odd length
     if len(message) % 2 != 0:
         message += ' '
     return [code(message[i]) << 6 | code(message[i + 1]) for i in range(0, len(message) - 1, 2)]
 
 
+# get char code relative to cyrillic alphabet
 def code(char):
     return 0 if char.isspace() else ord(char) - START_CYRILLIC_CODE + 1
 
 
+# get unicode char for given relative to cyrillic code
 def char(code):
     return ' ' if not code else chr(code + START_CYRILLIC_CODE - 1)
 
